264bda6a73bc24342154c3a5c19767eed5089db8
Michael Mongelli <45768887+MMongelli99@users.noreply.github.com>
2021-10-20 15:39:08 -0400

    Create credentials.py

---
+++ src/credentials.py
@@ -0,0 +1,12 @@
+# DO NOT SHARE YOUR CREDENTIALS
+
+# Login into your Spotify developer account at https://developer.spotify.com.
+# Create an app and visit the Dashboard, go to the app's overview to retrieve the 'Client ID' and 'Client Secret' values.
+# Put these values into the variables below.
+
+client_id = ''
+client_secret = ''
+username = '' # Your official Spotify account username.
+
+# Then go to the Spotify developer app's Settings and set the field called 'Redirect URIs' with this value:
+redirect_uri = 'http://localhost:8080/'
