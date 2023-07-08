e217a6abb0f40e7f08b858a851a199621104fee5
Michael Mongelli <45768887+MMongelli99@users.noreply.github.com>
2023-07-08 00:43:31 -0400

    Create spotify-top-track-hist.bat

---
+++ src/spotify-top-track-hist.bat
@@ -0,0 +1,8 @@
+:: If you're on Windows, save this file as a shortcut and pin it to your task bar so you can save your recent favorites whenever you want with just a click
+:: Replace the paths below with your Python executable (preferably in a venv) and the main.py file of this repo
+
+@echo off
+
+"path\to\python.exe" "path\to\src\main.py"
+
+pause
